a9a4cbe107bb63986808012404ecfe2138ebe20f
Sherlok <sergei_malin@yahoo.com>
2022-02-12 16:06:18 -0600

    add styles

--- app.js
+++ app.js
@@ -12,3 +12,13 @@ questions.forEach(function (question) {
     });
   });
 });
+
+// const btns = document.querySelectorAll(".question-btn");
+
+// btns.forEach(function (btn) {
+//   btn.addEventListener("click", function () {
+//     let styles = btn.parentElement.parentElement;
+
+//     styles.classList.toggle("show-question");
+//   });
+// });
